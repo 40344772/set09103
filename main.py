fa44c4dc2f65598be60ea3da4440ff45390f3d62
403447722 <40344772@live.npaier.ac.uk>
2020-11-28 23:34:37 +0000

    pushing working done

--- main.py
+++ main.py
@@ -3,36 +3,44 @@ app = Flask(__name__)
 
 @app.route('/login')
 def Login_page():
-	return render_template('hello.html')
+	return render_template('loginPage.html'), 200
 
 @app.route('/student/home')
 def student_home_page():
-        return 'Hello Napier'
+        return render_template('studentMain.html'), 200
 
 @app.route('/student/work')
 def student_work_page():
-        return 'Hello Napier'
+        return render_template('work.html'), 200
 
 @app.route('/student/feedback')
 def student_feedback_page():
-        return 'Hello Napier'
+        return render_template('feedback.html'), 200
 
 @app.route('/student/load')
 def student_load_page():
-        return 'Hello Napier'
+        return render_template('load.html'), 200
+
+@app.route('/student/submit')
+def student_submit_page():
+        return render_template('submit.html'), 200
 
 @app.route('/staff/home')
 def staff_home_page():
-        return 'Hello Napier'
+        return render_template('techerHome.html'), 200
 
 @app.route('/staff/work')
 def staff_work_page():
-        return 'Hello Napier'
+        return render_template('workTeacher.html'), 200
 
 @app.route('/staff/marking')
 def staff_marking_page():
-        return 'Hello Napier'
+        return render_template('submitTeacher.html'), 200
 
 @app.route('/staff/load')
 def staff_load_page():
-        return 'Hello Napier'
+       return render_template('loadTeacher.html'), 200
+
+@app.route('/staff/upload')
+def staff_upload_page():
+        return render_template('upload.html'), 200
